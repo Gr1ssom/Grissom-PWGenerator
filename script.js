908d7c72387a428f6a09950900ffbbadf244c08a
Dylan Grissom <dgriss295@gmail.com>
2023-02-27 17:28:15 -0600

    First Commit

---
+++ script.js
@@ -0,0 +1,72 @@
+var characterLength = 8;
+var choiceArr = [];
+var specialCharArr = ['!','@','#','$','%','^','&','*'];
+var lowerCaseArr = ['a','b','c','d','e','f','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numberArr = ['0','1','2','3','4','5','6','7','8','9'];
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var correctPrompts = getPrompts();
+  var passwordText = document.querySelector("#password");
+
+  if(correctPrompts) {
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+  } else {
+    passwordText.value = "";
+  }
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+function generatePassword() {
+ var password = "";
+ for (var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomIndex];
+ }
+ return password;
+}
+
+
+// PROMPTS ARE COMPLETED BELOW
+
+
+function getPrompts() {
+  choiceArr = [];
+
+  characterLength = parseInt(prompt("How many characters for your password? (8-128 characters)"));
+    if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+      alert("Password must be between 8 - 128 characters long. Please try again.");
+      return false;
+    }
+
+    if(confirm("Would you like to include lowercase letters in your password?")) {
+      choiceArr = choiceArr.concat(lowerCaseArr);
+    }
+
+    if(confirm("Would you like to include uppercase letters in your password?")) {
+      choiceArr = choiceArr.concat(upperCaseArr);
+    }
+
+    if(confirm("Would you like to include symbols in your password?")) {
+      choiceArr = choiceArr.concat(specialCharArr);
+    }
+
+    if(confirm("Would you like to include numbers in your password?")) {
+      choiceArr = choiceArr.concat(numberArr);
+    }
+
+    return true;
+  }
+
